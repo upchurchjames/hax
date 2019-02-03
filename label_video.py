0695678d1b4846f655ee08653465f989a6cd1cdc
James <jamesgupchurch@knights.ucf.edu>
2019-02-02 21:12:47 -0500

    Flexin on those normal cameras

--- label_video.py
+++ label_video.py
@@ -1,42 +1,89 @@
-import io
-import os
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse
+import imutils
+import time
+import numpy as np
+import cv2
 
-from google.cloud import videointelligence
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "uc.json"
+def start():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--prototxt", required=True,
+                    help="pat to Caffe 'deploy' prototxt file")
+    ap.add_argument("-m", "--model", required=True,
+                    help="path to Caffe pre-trained model")
+    ap.add_argument("-c", "--confidence", type=float, default=0.2,
+                    help="minimum probability to filter weak detections")
+    args = vars(ap.parse_args())
 
-video_client = videointelligence.VideoIntelligenceServiceClient()
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+               "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+               "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+               "sofa", "train", "tvmonitor"]
+    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-path = 'cars.mp4'
+    net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-with io.open(path, 'rb') as movie:
-    input_content = movie.read()
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+    fps = FPS().start()
 
-features = [videointelligence.enums.Feature.LABEL_DETECTION]
+    while True:
+        # grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels
+        frame = vs.read()
 
-operation = video_client.annotate_video(
-    features=features, input_content=input_content)
-print('\nProcessing video for label annotations:')
+        frame = imutils.resize(frame, width=400)
 
-result = operation.result(timeout=120)
-print('\nFinished processing.')
+        # grab the frame dimensions and convert it to a blob
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                                     0.007843, (300, 300), 127.5)
 
-# first result is retrieved because a single video was processed
-segment_labels = result.annotation_results[0].segment_label_annotations
-for i, segment_label in enumerate(segment_labels):
-    print('Video label description: {}'.format(
-        segment_label.entity.description))
-    for category_entity in segment_label.category_entities:
-        print('\tLabel category description: {}'.format(
-            category_entity.description))
+        # pass the blob through the network and obtain the detections and
+        # predictions
+        net.setInput(blob)
+        detections = net.forward()
 
-    for i, segment in enumerate(segment_label.segments):
-        start_time = (segment.segment.start_time_offset.seconds +
-                      segment.segment.start_time_offset.nanos / 1e9)
-        end_time = (segment.segment.end_time_offset.seconds +
-                    segment.segment.end_time_offset.nanos / 1e9)
-        positions = '{}s to {}s'.format(start_time, end_time)
-        confidence = segment.confidence
-        print('\tSegment {}: {}'.format(i, positions))
-        print('\tConfidence: {}'.format(confidence))
-    print('\n')
+        for i in np.arange(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+            confidence = detections[0, 0, i, 2]
+
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence > args["confidence"]:
+                # extract the index of the class label from the
+                # `detections`, then compute the (x, y)-coordinates of
+                # the bounding box for the object
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                                             confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+            # show the output frame
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
+
+            # update the FPS counter
+            fps.update()
+    cv2.destroyAllWindows()
+    vs.stop()
+    pass
+
+
+if __name__ == '__main__':
+    start()
